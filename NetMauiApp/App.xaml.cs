4df7e43b5f30a5c39035871674a365621706fefa
kwonsss <sh.kwon@dhandy.co.kr>
2024-11-06 14:22:35 +0900

    Maui Blazor

---
+++ NetMauiApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace NetMauiApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ NetMauiApp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace NetMauiApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
